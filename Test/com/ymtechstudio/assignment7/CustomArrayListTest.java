9d3ce4dc88364f2b19ba95c455a0505856cadeee
Yasiin <yasiinmuhammad@gmail.com>
2020-09-02 17:19:56 -0700

    Fixed @Test

--- Test/com/ymtechstudio/assignment7/CustomArrayListTest.java
+++ Test/com/ymtechstudio/assignment7/CustomArrayListTest.java
@@ -8,12 +8,16 @@ import org.junit.jupiter.api.Test;
 class CustomArrayListTest {
 	// Test- Driven development methodology
 	// Step 1 write failing test
-	// Step 2 write business logic to make test past
+	// Step 2 write business logic to make test pass
 	// Reactor your code "rinse and repeat
+	// System Under Test or sut = myCustomList
 	CustomList<Integer> myCustomList;
+	// @BeforeEach and @BeforeAll are the JUnit 5 equivalents of @Before and
+	// @BeforeClass. These annotations were renamed with clearer names to avoid
+	// confusion.
 
 	@BeforeEach
-
+	// setUp() method, which runs before every test invocation.
 	void setUp() {
 		myCustomList = new CustomArrayList<>();
 		for (int i = 0; i < 99; i++) {
@@ -22,19 +26,31 @@ class CustomArrayListTest {
 	}
 
 	@Test
-	void should_add_one_item_to_list() {
+	void test_should_add_one_item_to_list() {
 		// Three A's
-		// Arrange, Act , Assert
+		// Arrange,
+		// Act
 		myCustomList.add(5);
 
 		// Assert
+		// What you expect and what would actually happen
+		// assertEquals() method compares tow values: the expected result and the actual
+		// value returned from the test code
 		assertEquals(5, myCustomList.get(4));
 		assertEquals(100, myCustomList.getSize());
 
 	}
 
 	@Test
-	void should_remove_one_item_to_list() {
+	void test_should_add_to_middle_of_list() {
+		myCustomList.add(5, 10);
+
+		assertEquals(10, myCustomList.get(5));
+
+	}
+
+	@Test
+	void test_should_remove_one_item_to_list() {
 		// Three A's
 		// Arrange, Act , Assert
 		Integer itemRemoved = myCustomList.remove(0);
@@ -48,7 +64,7 @@ class CustomArrayListTest {
 	}
 
 	@Test
-	void should_remove_form_middle_of_list() {
+	void test_should_remove_form_middle_of_list() {
 		// Three A's
 		// Arrange, Act , Assert
 
@@ -63,7 +79,7 @@ class CustomArrayListTest {
 	}
 
 	@Test
-	void should_remove_from_end_item_to_list() {
+	void test_should_remove_from_end_item_to_list() {
 		// Three A's
 		// Arrange, Act , Assert
 
@@ -76,4 +92,5 @@ class CustomArrayListTest {
 		assertEquals(98, myCustomList.get(myCustomList.getSize() - 1));
 
 	}
+
 }
